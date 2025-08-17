638f2ce8a60a92a820f018488d6aa8e3dee820da
tuchnyak <georgii.o.sv@gmail.com>
2025-08-17 16:15:12 +0400

    feat(parser): add flexmark dependency

--- src/bld/java/net/tuchnyak/PersonalHubBuild.java
+++ src/bld/java/net/tuchnyak/PersonalHubBuild.java
@@ -14,27 +14,28 @@ public class PersonalHubBuild extends WebProject {
         name = "personal-hub";
         mainClass = "net.tuchnyak.PersonalHubSite";
         uberJarMainClass = "net.tuchnyak.PersonalHubSiteUber";
-        version = version(0,0,1);
+        version = version(0, 0, 2);
 
         downloadSources = true;
         autoDownloadPurge = true;
 
         repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES);
         scope(compile)
-            .include(dependency("com.uwyn.rife2", "rife2", version(1,9,1)))
-            .include(dependency("org.hsqldb", "hsqldb", version(2,7,4)))
-            .include(dependency("org.slf4j", "slf4j-api", version(2,0,16)));
+                .include(dependency("com.uwyn.rife2", "rife2", version(1, 9, 1)))
+                .include(dependency("org.hsqldb", "hsqldb", version(2, 7, 4)))
+                .include(dependency("com.vladsch.flexmark", "flexmark-all", version(0, 64, 0)))
+                .include(dependency("org.slf4j", "slf4j-api", version(2, 0, 16)));
         scope(test)
-            .include(dependency("org.jsoup", "jsoup", version(1,18,3)))
-            .include(dependency("org.junit.jupiter", "junit-jupiter", version(5,11,4)))
-            .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1,11,4)));
+                .include(dependency("org.jsoup", "jsoup", version(1, 18, 3)))
+                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 11, 4)))
+                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 11, 4)));
         scope(standalone)
-            .include(dependency("org.eclipse.jetty.ee10", "jetty-ee10", version(12,0,16)))
-            .include(dependency("org.eclipse.jetty.ee10", "jetty-ee10-servlet", version(12,0,16)))
-            .include(dependency("org.slf4j", "slf4j-simple", version(2,0,16)));
+                .include(dependency("org.eclipse.jetty.ee10", "jetty-ee10", version(12, 0, 16)))
+                .include(dependency("org.eclipse.jetty.ee10", "jetty-ee10-servlet", version(12, 0, 16)))
+                .include(dependency("org.slf4j", "slf4j-simple", version(2, 0, 16)));
 
         precompileOperation()
-            .templateTypes(HTML);
+                .templateTypes(HTML);
     }
 
     public static void main(String[] args) {
